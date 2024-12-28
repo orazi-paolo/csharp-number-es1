0eda4da459dc59d8a24337c284f5e10392499a12
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-28 18:48:32 +0100

    first commit

---
+++ csharp-number-es1/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+Console.WriteLine("Inserisci il primo numero");
+string? input1 = Console.ReadLine();
+
+Console.WriteLine("Inserisci il secondo numero");
+string? input2 = Console.ReadLine();
+
+int firstNumber;
+int secondNumber;
+
+bool conversioneRiuscita1 = int.TryParse(input1, out firstNumber);
+bool conversioneRiuscita2 = int.TryParse(input2, out secondNumber);
+
+if (conversioneRiuscita1 && conversioneRiuscita2)
+{
+    int sum = firstNumber + secondNumber;
+    WriteLine($"La somma è: {sum}");
+}
+else
+{
+    WriteLine("Uno dei due valori inseriti non è un numero");
+}
